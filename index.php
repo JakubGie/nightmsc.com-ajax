cdd67e7b7bb2dec6629a2e55fea01532612bc84a
Jakub <jakubgielara@gmail.com>
2022-02-28 16:14:47 +0100

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Night Music - feel the night!</title>
+
+    <?php include 'elements/head.php'; ?>
+
+</head>
+<body>
+    <img src="img/NMSClogowhite.png" class="logo" alt="Night Music Logo">
+    <form>
+        <h2>SUBMIT YOUR TRACK</h2>
+        <input type="text" placeholder="Your Artist Name">
+        <input type="text" placeholder="Your Email">
+        <input type="text" placeholder="Name of Song">
+        <input type="text" placeholder="Listen Link (Soundcloud, Dropbox, etc)">
+        <textarea rows="1" placeholder="Any additional info"></textarea>
+        <input type="submit" value="SEND">
+    </form>
+</body>
+</html>
