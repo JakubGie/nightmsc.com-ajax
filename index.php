f6ffffd7f685e95ebc26f14aa34b7021b72bc7e5
Jakub <jakubgielara@gmail.com>
2022-02-28 16:46:49 +0100

    vertical center

--- index.php
+++ index.php
@@ -8,15 +8,19 @@
 
 </head>
 <body>
-    <img src="img/NMSClogowhite.png" class="logo" alt="Night Music Logo">
-    <form>
-        <h2>SUBMIT YOUR TRACK</h2>
-        <input type="text" placeholder="Your Artist Name">
-        <input type="text" placeholder="Your Email">
-        <input type="text" placeholder="Name of Song">
-        <input type="text" placeholder="Listen Link (Soundcloud, Dropbox, etc)">
-        <textarea rows="1" placeholder="Any additional info"></textarea>
-        <input type="submit" value="SEND">
-    </form>
+    <div class="outer">
+        <div class="middle">
+            <img src="img/NMSClogowhite.png" class="logo" alt="Night Music Logo">
+            <form>
+                <h2>SUBMIT YOUR TRACK</h2>
+                <input type="text" placeholder="Your Artist Name">
+                <input type="text" placeholder="Your Email">
+                <input type="text" placeholder="Name of Song">
+                <input type="text" placeholder="Listen Link (Soundcloud, Dropbox, etc)">
+                <textarea rows="1" placeholder="Any additional info"></textarea>
+                <input type="submit" value="SEND">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
