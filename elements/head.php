667e7483023374ff68082509ae210126def5991d
Jakub <jakubgielara@gmail.com>
2022-03-03 19:41:43 +0100

    brand

--- elements/head.php
+++ elements/head.php
@@ -4,7 +4,7 @@
 
 <meta http-equiv="X-Ua-Compatible" content="IE=edge">
 
-<link rel="shortcut icon" href="img/NMSClogosignwhite.png">
+<link rel="shortcut icon" href="https://nightmsc.com/img/NMSClogosignwhite.ico">
 
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
